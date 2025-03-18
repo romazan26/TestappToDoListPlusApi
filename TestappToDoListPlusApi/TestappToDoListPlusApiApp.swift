3f8ff77c927ca5728bddcf87a256bbaa4a0e6c9f
Roman <romazan.26@gmail.com>
2025-03-18 14:12:59 +0300

    Initial Commit

---
+++ TestappToDoListPlusApi/TestappToDoListPlusApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestappToDoListPlusApiApp.swift
+//  TestappToDoListPlusApi
+//
+//  Created by Роман Главацкий on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestappToDoListPlusApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
